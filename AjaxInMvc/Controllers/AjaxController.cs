fdb4828bfc340c197c5438c872e7443c0d8d77ab
Thuyvk's Blog <khanhthuy46k2dhv@gmail.com>
2017-07-27 16:12:59 +0700

    first commit

---
+++ AjaxInMvc/Controllers/AjaxController.cs
@@ -0,0 +1,42 @@
+﻿using AjaxInMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxInMvc.Controllers
+{
+    public class AjaxController : Controller
+    {
+        
+        public List<Product> InitData()
+        {
+            List<Product> prodList = new List<Product>();
+
+            Product p1 = new Product { ProdCode = "P001", ProdName = "Mobile", ProdQty = 75 };
+            Product p2 = new Product { ProdCode = "P002", ProdName = "Laptop", ProdQty = 125 };
+            Product p3 = new Product { ProdCode = "P003", ProdName = "Netbook", ProdQty = 100 };
+            prodList.Add(p1);
+            prodList.Add(p2);
+            prodList.Add(p3);
+
+            return prodList;
+        }
+
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult ShowDetails()
+        {
+            System.Threading.Thread.Sleep(3000);
+            string code = Request.Form["ProductCode"];
+            var products = InitData();
+            Product objView = products.Where(x => x.ProdCode == code).FirstOrDefault();
+            return PartialView("_ShowDetails", objView);
+        }
+    }
+}
---
+++ AjaxInMvc/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace AjaxInMvc.Models
+{
+    public class Product
+    {
+        public string ProdCode
+        {
+            get;
+            set;
+        }
+        public string ProdName
+        {
+            get;
+            set;
+        }
+        public int ProdQty
+        {
+            get;
+            set;
+        }
+    }
+}
